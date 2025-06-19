1185c730ea31f8d87af5487a15ec06846115a31d
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-06-19 10:52:24 +0200

    Update SkyBlock.java
    Co-Authored-By: Eli <155769169+Elisz11@users.noreply.github.com>

--- src/main/java/org/leafhold/skyBlock/SkyBlock.java
+++ src/main/java/org/leafhold/skyBlock/SkyBlock.java
@@ -1,12 +1,35 @@
 package org.leafhold.skyBlock;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkyBlock extends JavaPlugin {
+    private static boolean isSpigot;
+    private static boolean isPaper;
+
+    @Override
+    public void onLoad() {
+        try {
+            Class.forName("org.spigotmc.SpigotConfig");
+            isSpigot = true;
+        } catch (ClassNotFoundException e) {
+            this.getLogger().severe("This plugin requires Spigot or Paper to run!");
+            isSpigot = false;
+            return;
+        }
+        try {
+            Class.forName("com.destroystokyo.paper.PaperConfig");
+            isPaper = true;
+        } catch (ClassNotFoundException e) {
+            isPaper = false;
+        }
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        if (!isSpigot) {
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
 
     }
 
