a714a63d9b57d41485fdde14dcf6b57791be5d40
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-06-23 03:30:19 +0200

    Add Citizens repository to build.gradle

--- src/main/java/org/leafhold/lhSkyBlock/commands/ShopCommand.java
+++ src/main/java/org/leafhold/lhSkyBlock/commands/ShopCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import org.leafhold.lhSkyBlock.utils.DatabaseManager;
 import org.leafhold.lhSkyBlock.lhSkyBlock;
@@ -16,8 +17,6 @@ import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
 
 import java.util.UUID;
-
-import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 
 public class ShopCommand implements CommandExecutor, Listener {
