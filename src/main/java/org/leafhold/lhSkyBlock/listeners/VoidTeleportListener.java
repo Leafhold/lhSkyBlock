8f992c0a64f64421af288f84e3d9e8efdc8d23b0
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-06-25 15:25:30 +0200

    Refactor VoidTeleportListener to use async teleportation and add world check

--- src/main/java/org/leafhold/lhSkyBlock/listeners/VoidTeleportListener.java
+++ src/main/java/org/leafhold/lhSkyBlock/listeners/VoidTeleportListener.java
@@ -4,6 +4,7 @@ import org.leafhold.lhSkyBlock.lhSkyBlock;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,9 +13,11 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 public class VoidTeleportListener implements Listener {
     private final lhSkyBlock plugin;
+    private static FileConfiguration config;
 
     public VoidTeleportListener(lhSkyBlock plugin) {
         this.plugin = plugin;
+        config = plugin.getConfig();
     }
 
     @EventHandler
@@ -24,8 +27,14 @@ public class VoidTeleportListener implements Listener {
             if (event.getCause() == DamageCause.VOID) {
                 event.setCancelled(true);
                 World world = player.getWorld();
-                Location loc = world.getSpawnLocation();
-                player.teleport(loc);
+
+                if (world != null && world.getName().equals(config.getString("main_world"))) {
+                    Location loc = world.getSpawnLocation();
+                    loc.add(0.5, 1, 0.5);
+                    loc.setPitch(0);
+                    loc.setYaw(180);
+                    player.teleportAsync(loc);
+                }
             }
         }
     }
