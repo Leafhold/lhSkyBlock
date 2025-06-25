186c07ed4c1988be390dc12552a59acdc66bb6c7
LookAtFr3sn0 <61255110+LookAtFr3sn0@users.noreply.github.com>
2025-06-25 16:28:32 +0200

    Player spawn manager (#15)
    * Add VoidTeleportListener to handle player damage from void
    
    * Add VoidTeleportListener to prevent player damage from void and teleport to spawn
    
    * Refactor VoidTeleportListener to use async teleportation and add world check
    
    * Rename default_world to main_world in configuration file
    
    * Edit VoidTeleportListener to ensure players are teleported to spawn location with proper teleport cause on join and void damage
    
    * Fix world name comparison to be case-insensitive and adjust spawn location offset in VoidTeleportListener
    
    * Refactor null world check in VoidTeleportListener to improve teleportation logic
    
    ---------
    
    Signed-off-by: LookAtFr3sn0 <61255110+LookAtFr3sn0@users.noreply.github.com>

--- src/main/java/org/leafhold/lhSkyBlock/lhSkyBlock.java
+++ src/main/java/org/leafhold/lhSkyBlock/lhSkyBlock.java
@@ -7,6 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.TabCompleter;
 
 import org.leafhold.lhSkyBlock.commands.IslandCommand;
+import org.leafhold.lhSkyBlock.listeners.VoidTeleportListener;
 import org.leafhold.lhSkyBlock.commands.ShopCommand;
 import org.leafhold.lhSkyBlock.utils.DatabaseManager;
 
@@ -59,6 +60,8 @@ public final class lhSkyBlock extends JavaPlugin {
         getCommand("island").setExecutor(islandCommand);
         getCommand("island").setTabCompleter(islandCommand);
         getServer().getPluginManager().registerEvents(islandCommand, instance);
+      
+        getServer().getPluginManager().registerEvents(new VoidTeleportListener(instance), instance);
 
         ShopCommand shopCommand = new ShopCommand(instance);
         getCommand("shop").setExecutor(shopCommand);
