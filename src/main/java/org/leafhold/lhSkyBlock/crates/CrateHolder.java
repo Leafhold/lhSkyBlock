975a361667d0cb791d0e2ccf0d493ea1459c594d
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-07-11 16:06:28 +0200

    Add CrateHolder class and initial crate configuration in crates.yml

---
+++ src/main/java/org/leafhold/lhSkyBlock/crates/CrateHolder.java
@@ -0,0 +1,12 @@
+package org.leafhold.lhSkyBlock.crates;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class CrateHolder implements InventoryHolder {
+
+    @Override
+    public Inventory getInventory() {
+        return null;
+    }
+}
